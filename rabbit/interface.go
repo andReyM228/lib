19f6d60e80a45c8b11720fa782a565cdd9051e99
Andrey Morg <a.o.morgun@gmail.com>
2023-08-20 23:28:31 +0300

    add prepare-publish for 2pc

--- rabbit/interface.go
+++ rabbit/interface.go
@@ -6,4 +6,5 @@ type Rabbit interface {
 	CloseConnection() error
 	Reply(queueName string, statusCode int64, message interface{}) error
 	Request(queueName string, message interface{}) (ResponseModel, error)
+	PreparePublish(queueName string, message interface{}) error
 }
