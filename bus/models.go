a3f4536b3aa1bd27e1c5155784fcde20dbb79c91
Oleksandr Bokov <bokoff2020@gmail.com>
2023-09-01 19:17:59 +0300

    validate bus/models

--- bus/models.go
+++ bus/models.go
@@ -2,12 +2,12 @@ package bus
 
 type (
 	LoginRequest struct {
-		ChatID   int64  `json:"chat_id"`
-		Password string `json:"password"`
+		ChatID   int64  `json:"chat_id" validate:"required"`
+		Password string `json:"password" validate:"required"`
 	}
 
 	LoginResponse struct {
-		UserID int64 `json:"user_id"`
+		UserID int64 `json:"user_id" validate:"required"`
 	}
 
 	GetUserByIDRequest struct {
